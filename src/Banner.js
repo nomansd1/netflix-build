7a2ec83500d0d28303d683f82dfdfc3eeba621e2
Syed Noman Ali <66224759+nomansd1@users.noreply.github.com>
2021-03-06 02:49:07 +0500

    fetch api error updated

--- src/Banner.js
+++ src/Banner.js
@@ -9,13 +9,16 @@ function Banner() {
     
     useEffect(()=> {
         async function fetchData() {
-            const response = await axios.get(requests.fetchTrending);
+            const response = await axios.get("https://api.themoviedb.org/3/discover/tv?api_key=f81980ff410e46f422d64ddf3a56dddd&with_networks=213");
+            // const response = await fetch("https://api.themoviedb.org/3/discover/tv?api_key=f81980ff410e46f422d64ddf3a56dddd&with_networks=213");
+            // const apiData = response.json(); 
             setMovie(
                 response.data.results[
                     Math.floor(Math.random() * response.data.results.length - 1)
                 ]
             );
             return response;
+            // console.log(apiData);
         }
 
         fetchData();
@@ -32,29 +35,22 @@ function Banner() {
         <header
         className="banner"
         style={{
-            backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+            // backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+            backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
             backgroundSize: "cover",
             backgroundPosition: "center center", 
         }}
         >
             <div className="banner__contents">
-                <h1 className="banner__title">Movie Name</h1>
+                <h1 className="banner__title">
+                    {movie?.title || movie?.name || movie?.original_name }
+                </h1>
                 <div className="banner__buttons">
                     <button className="banner__button">Play</button>
                     <button className="banner__button">My List</button>
                 </div>
                 <h1 className="banner__description">
-                {truncate( `this is the test description this is the test description
-                this is the test description this is the test description
-                this is the test description this is the test description
-                this is the test description this is the test description
-                this is the test description this is the test description
-                this is the test description this is the test description
-                this is the test description this is the test description
-                this is the test description this is the test description
-                this is the test description this is the test description
-                this is the test description this is the test description
-                this is the test description this is the test description`, 120)}
+                {truncate( movie?.overview, 120)}
                 </h1>
             </div>
             <div className="banner--fadeBottom"/>
--- src/axios.js
+++ src/axios.js
@@ -12,10 +12,6 @@ const BASE_URL = "https://api.themoviedb.org/3";
 
 const instance = axios.create({
     baseURL: BASE_URL,
-    headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Content-Type": "text/plain",
-    },
 })
 
 export default instance;
